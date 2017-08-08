a1b36e285a237b55f14b927c314251268afa9f22
Christophe Nouguier <christophe.nouguier@free.fr>
2017-08-08 23:22:54 +0200

    Added listener to update the current organisation when the user changed

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,6 @@
 import logger from 'loglevel'
+import { Events } from 'quasar'
+import { Store } from 'kCore/client'
 
 // We faced a bug in babel so that transform-runtime with export * from 'x' generates import statements in transpiled code
 // Tracked here : https://github.com/babel/babel/issues/2877
@@ -8,6 +10,14 @@ export * as mixins from './mixins'
 
 export default function init () {
   // const app = this
+
+  Events.$on('user-changed', user => {
+    if (user.organisations && user.organisations.length > 0) {
+      Store.set('organisation', user.organisations[0])
+    } else {
+      logger.debug('empty organisations')
+    }
+  })
 
   logger.debug('Initializing kaelia team')
 }
