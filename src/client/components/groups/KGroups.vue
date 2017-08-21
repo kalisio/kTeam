463223f068fc73f788492655daab31146d04eeff
christophe.nouguier@free.fr <christophe.nouguier@free.fr>
2017-08-21 16:19:30 +0200

    Added babel-polyfill

--- src/client/components/groups/KGroups.vue
+++ src/client/components/groups/KGroups.vue
@@ -1,14 +1,13 @@
 <template>
-  <!--k-collection 
+  <k-collection 
   :service="'groups'"
   :context="context"
   :actions="actions" 
-  @actionRequested="onActionRequested" /-->
-  <div>Hello</div>
+  @actionRequested="onActionRequested" />
 </template>
 
 <script>
-//import { mixins } from 'kCore/client'
+import { mixins } from 'kCore/client'
 
 export default {
   name: 'k-groups',
@@ -18,17 +17,17 @@ export default {
       context: null
     }
   },
- /* mixins: [
+ mixins: [
     mixins.collection.baseItemAction,
     mixins.collection.createItem,
     mixins.collection.deleteItem,
     mixins.collection.editItem
-  ],*/
+  ],
   created () {
     let Store = this.store()
     // Load the required component
-    //let loadComponent = Store.get('loadComponent')
-    //this.$options.components['k-collection'] = loadComponent('collection/KCollection')
+    let loadComponent = Store.get('loadComponent')
+    this.$options.components['k-collection'] = loadComponent('collection/KCollection')
     // Retrieve the context for the groups service
     this.context = Store.get(Store.get('config.users.context'), null)
   }
