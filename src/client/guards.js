06d00ded6afbdfb8934cbbe9cd29ed698aa7c313
Luc Claustres <luc.claustres@orange.fr>
2017-09-18 22:54:58 +0200

    Fixed authorisation guard

--- src/client/guards.js
+++ src/client/guards.js
@@ -3,10 +3,6 @@ import { hasServiceAbilities, hasResourceAbilities } from '../permissions'
 // Guard unauthorised users
 export function authorisationGuard (app) {
   return function authorisationGuard (user, to, from, next) {
-    // Unauthenticated cannot be authorised
-    if (!user) {
-      return false
-    }
     // Check for access to service first
     if (to.params.service) {
       if (to.params.context) {
@@ -16,12 +12,12 @@ export function authorisationGuard (app) {
       } else if (!hasServiceAbilities(user.abilities, app.getServicePath(to.params.service))) {
         return false
       }
-
       // Then check for access to operation
       if (to.params.action && !hasResourceAbilities(user.abilities, to.params.action, to.params.service)) {
         return false
       }
     }
+
     return true
   }
 }
