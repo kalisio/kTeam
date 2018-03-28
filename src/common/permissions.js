4c199941232a20bc93e667af60a397d3943c54e5
Luc Claustres <luc.claustres@orange.fr>
2018-03-28 21:32:50 +0200

    Try to fix issue appearing when an old token is used and the user not correctly retrieved

--- src/common/permissions.js
+++ src/common/permissions.js
@@ -10,27 +10,29 @@ export function defineOrganisationAbilities (subject, can, cannot) {
 
     if (subject.organisations) {
       subject.organisations.forEach(organisation => {
-        // Generic rules for resources
-        permissions.defineResourceRules(subject, organisation, 'organisations', can)
-        // Specific rules for organisations
-        const role = permissions.Roles[organisation.permissions]
-        if (role >= permissions.Roles.member) {
-          // The unique identifier of a service is its path not its name.
-          // Indeed we have for instance a 'groups' service in each organisation.
-          can('service', organisation._id.toString() + '/members')
-          can('read', 'members', { context: organisation._id })
-          can('service', organisation._id.toString() + '/tags')
-          can('read', 'tags', { context: organisation._id })
-          can('service', organisation._id.toString() + '/groups')
-          can('service', organisation._id.toString() + '/storage')
-          can(['read', 'create', 'remove'], 'storage', { context: organisation._id })
-        }
-        if (role >= permissions.Roles.manager) {
-          // The unique identifier of a service is its path not its name.
-          // Indeed we have for instance a 'groups' service in each organisation.
-          can('update', 'members', { context: organisation._id })
-          can('create', 'groups', { context: organisation._id })
-          can(['create', 'remove'], 'tags', { context: organisation._id })
+        if (organisation._id) {
+          // Generic rules for resources
+          permissions.defineResourceRules(subject, organisation, 'organisations', can)
+          // Specific rules for organisations
+          const role = permissions.Roles[organisation.permissions]
+          if (role >= permissions.Roles.member) {
+            // The unique identifier of a service is its path not its name.
+            // Indeed we have for instance a 'groups' service in each organisation.
+            can('service', organisation._id.toString() + '/members')
+            can('read', 'members', { context: organisation._id })
+            can('service', organisation._id.toString() + '/tags')
+            can('read', 'tags', { context: organisation._id })
+            can('service', organisation._id.toString() + '/groups')
+            can('service', organisation._id.toString() + '/storage')
+            can(['read', 'create', 'remove'], 'storage', { context: organisation._id })
+          }
+          if (role >= permissions.Roles.manager) {
+            // The unique identifier of a service is its path not its name.
+            // Indeed we have for instance a 'groups' service in each organisation.
+            can('update', 'members', { context: organisation._id })
+            can('create', 'groups', { context: organisation._id })
+            can(['create', 'remove'], 'tags', { context: organisation._id })
+          }
         }
       })
     }
@@ -42,9 +44,11 @@ export function defineGroupAbilities (subject, can, cannot) {
   if (subject) {
     if (subject.groups) {
       subject.groups.forEach(group => {
-        // Generic rules for resources
-        permissions.defineResourceRules(subject, group, 'groups', can)
-        // No specific rules for groups
+        if (group._id) {
+          // Generic rules for resources
+          permissions.defineResourceRules(subject, group, 'groups', can)
+          // No specific rules for groups
+        }
       })
     }
   }
--- test/index.test.js
+++ test/index.test.js
@@ -90,6 +90,8 @@ describe('kTeam', () => {
     server = app.listen(port)
     server.once('listening', _ => done())
   })
+  // Let enough time to process
+  .timeout(5000)
 
   it('unregistered users cannot create organisations', (done) => {
     orgService.create({ name: 'test-org' }, { checkAuthorisation: true })
