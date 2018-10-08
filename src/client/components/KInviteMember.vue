0c14a7e541c0747e25b422fc0edc6767db4868b8
Christophe Nouguier <christophe.nouguier@free.fr>
2018-10-08 15:36:11 +0200

    WIP #34

--- src/client/components/KInviteMember.vue
+++ src/client/components/KInviteMember.vue
@@ -1,23 +1,59 @@
 <template>
   <k-modal ref="modal" :title="$t('KInviteMember.TITLE')" :toolbar="getToolbar()" :buttons="getButtons()" :route="true">
-    <div slot="modal-content" class="column xs-gutter">
-      <k-form ref="form" :schema="getSchema()" />
-    </div>
+    <q-tabs slot="modal-content" align="justify" v-model="mode" inverted>
+      <q-tab name="unique" icon="person" slot="title" />
+      <q-tab name="multiple" icon="people" slot="title" />
+      <q-tab-pane name="unique">
+        <k-form ref="form" :schema="getSchema()" />
+      </q-tab-pane>
+      <q-tab-pane name="multiple">
+        <q-field
+          :helper="$t('KInviteMember.FILE_FIELD_HELPER')"
+          :error-label="fileErrorLabel"
+          :error="fileError">
+            <k-input-file 
+              :mime-types="['txt/csv', 'application/vnd.ms-excel']"
+              :clearable="true"
+              @cleared="onInputFileCleared"
+              @rejected="onInputFileRejected" 
+              @failed="onInputFileFailed" 
+              @loaded="onInputFileLoaded" />
+        </q-field>
+      </q-tab-pane>
+    </q-tabs> 
   </k-modal>
 </template>
 
 <script>
-import { mixins as kCoreMixins, utils } from 'kCore/client'
+import _ from 'lodash'
+import { QTabs, QTab, QTabPane, QField, Dialog } from 'quasar'
+import { permissions as kCorePermissions, mixins as kCoreMixins, utils } from 'kCore/client'
+import Papa from 'papaparse'
 
 export default {
   name: 'k-invite-member',
   mixins: [
     kCoreMixins.refsResolver(['form'])
   ],
+  components: {
+    QTabs,
+    QTab,
+    QTabPane,
+    QField,
+    Dialog
+  },
   props: {
     contextId: {
       type: String,
       required: true
+    }
+  },
+  data () {
+    return {
+      mode: 'unique',
+      fileError: false,
+      fileErrorLabel: '',
+      fileContent: ''
     }
   },
   methods: {
@@ -74,40 +110,121 @@ export default {
       ]
     },
     doInvite (event, done) {
-      let result = this.$refs.form.validate()
-      if (result.isValid) {
-        // Add the locale information
-        result.values.locale = utils.getLocale()
-        // Add the sponsor information
-        result.values.sponsor = {
-          id: this.$store.get('user._id'),
-          organisationId: this.contextId,
-          roleGranted: result.values.role
-        }
-        // Remove the role from the form data
-        delete result.values.role
-        // Create the user
-        let usersService = this.$api.getService('users')
-        usersService.create(result.values)
-        .then(() => {
+      if (this.mode === 'unique') {
+        let result = this.$refs.form.validate()
+        if (result.isValid) this.doInviteUnique(result.values, done)
+        else done()
+      } else {
+        if (this.fileContent !== '') {
+          let result = Papa.parse(this.fileContent, { skipEmptyLines: true })
+          this.doInviteMultiple(result.data, done)
+        } else {
+          this.fileError = true
           done()
-          this.doClose()
+        }
+      }
+    },
+    async doInviteUnique (data, done) {
+      // Add the locale information
+      data.locale = utils.getLocale()
+      // Add the sponsor information
+      data.sponsor = {
+        id: this.$store.get('user._id'),
+        organisationId: this.contextId,
+        roleGranted: data.role
+      }
+      // Remove the role from the form data
+      delete data.role
+      // Create the user
+      let usersService = this.$api.getService('users')
+      await usersService.create(data)
+      done()
+      this.doClose()
+
+    },
+    async doInviteMultiple (data, done) {
+      let errors = []
+      let guests = []
+      let emailExpr = /\S+@\S+\.\S+/
+      for (let i = 0; i < data.length; i++) {
+        let record = data[i]
+        if (record.length === 3 && emailExpr.test(record[1]) && kCorePermissions.RoleNames.includes(record[2])) {
+          let guest = {
+            locale: utils.getLocale(),
+            sponsor: {
+              id: this.$store.get('user._id'),
+              organisationId: this.contextId,
+              roleGranted: record[2]
+            },
+            name: record[0],
+            email: record[1]
+          }
+          guests.push(guest)
+        }
+        else {
+          let error = {
+            line: i + 1,
+            record: record
+          } 
+          errors.push(error) 
+        }
+      }
+      if (guests.length === 0) {
+        Dialog.create({
+          title: this.$t('KInviteMember.ALERT_FILE_IMPORT_DIALOG'),
+          message: this.$t('KInviteMember.ALERT_FILE_IMPORT_MESSAGE'),
+          buttons: [
+            'Ok'
+          ]
         })
-        .catch(error => {
-          done(error)
+        done()
+      } else if (errors.length > 0) {
+        Dialog.create({
+          title: this.$t('KInviteMember.CONFIRM_FILE_IMPORT_DIALOG'),
+          message: this.$t('KInviteMember.CONFIRM_FILE_IMPORT_MESSAGE', { errors: errors.length, records: data.length }),
+          buttons: [
+            { label: 'Ok', handler: async () => {
+                let usersService = this.$api.getService('users')
+                for (let i = 0; i < guests.length; ++i) await usersService.create(guests[i])
+                done()
+                this.doClose()
+              }
+            },
+            { label: this.$t('KInviteMember.CANCEL_BUTTON'), handler: () => done() }
+          ]
         })
       } else {
+        let usersService = this.$api.getService('users')
+        for (let i = 0; i < guests.length; ++i) await usersService.create(guests[i])
         done()
+        this.doClose()
       }
     },
     doClose () {
       this.$refs.modal.close(() => this.$router.push({ name: 'members-activity' }))
+    },
+    onInputFileCleared () {
+      this.fileError = false
+      this.fileContent = ''
+    },
+    onInputFileRejected () {
+      this.fileError = true
+      this.fileErrorLabel = this.$t('KInviteMember.INVALID_FILE_TYPE')
+    },
+    onInputFileFailed () {
+      this.fileError = true
+      this.fileErrorLabel = this.$t('KInviteMember.ERROR_WHILE_LOADING_THE_FILE')
+    },
+    onInputFileLoaded (content) {
+      this.fileError = false
+      this.fileContent = content
     }
   },
   created () {
     // Load the required components
     this.$options.components['k-modal'] = this.$load('frame/KModal')
     this.$options.components['k-form'] = this.$load('form/KForm')
+    this.$options.components['k-input-file'] = this.$load('input/KInputFile')
   }
 }
 </script>
